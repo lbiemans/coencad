66ce0ad642851793187cd766a92e69b2d5965849
Lars Biemans <lbiemans@westeros.uvt.nl>
2020-03-16 14:38:29 +0100

    Mogelijke oplossing voor nieuwe connectie

--- coencadRDP_Tunnel.sh
+++ coencadRDP_Tunnel.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 # Get the input into a variable
 
 input=$1
@@ -22,9 +21,17 @@ read spassword
 echo "Host to connect to:"
 read connhost
 
+extraparams=$1
+
+# Cleanup if needed
+
+if [[ $extraparams == "y" ]] 
+ then echo "" >  ~/.config/freerdp/known_hosts
+  else
 # Create tunnel
 screen -d -m ssh $username@nc3.coencadcloud.nl -p 10222 -L 3389:$connhost:3389
 
 # Login to the server with RDP
-
 xfreerdp /u:$susername /p:$spassword /v:127.0.0.1 /f
+
+fi
